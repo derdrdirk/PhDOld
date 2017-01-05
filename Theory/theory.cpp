07a3526930feca1aeceaff3d025eba95f6257ee7
Knowledge <dirkhornung91@gmail.com>
2017-01-05 07:38:13 +0100

    added MINUIT2, changed compiler to clang++

--- Theory/theory.cpp
+++ Theory/theory.cpp
@@ -1,9 +1,34 @@
 #include "theory.h"
 
+
+double myFunction(double x, double y) {
+  return x*x*x*x + x*x + x*y + y*y;
+}
+
+void minuitFunction(int& nDim, double* gout, double& result, double par[], int flg) {
+  result = myFunction(par[0], par[1]);
+}
+
+
 Theory::Theory () {
   std::cout << "Theory..." << std::endl;
 }
 
 double Theory::Dimension0(double a) {
   return 1./4./M_PI * ( 1. + a );
+}
+
+void Theory::testMinuit() {
+
+
+//Fitter* minimizer = new TFitter(2);
+//
+//
+// double p1 =-1;
+// minimizer->ExecuteCommand("SET PRINTOUT", &p1,1);
+//
+// minimizer->SetFCN(minuitFunction);
+// minimizer->SetParameter(0,"X",2,1,0,0);
+// minimizer->SetParameter(1,"Y",2,1,0,0);
+//
 }
--- main.cpp
+++ main.cpp
@@ -3,12 +3,67 @@
 #include "Experiment/experiment.h"
 #include "Theory/theory.h"
 
+#include "Quad4F.h"
+#include "Minuit2/FunctionMinimum.h"
+#include "Minuit2/MnMigrad.h"
+#include "Minuit2/MnHesse.h"
+#include "Minuit2/MnUserParameters.h"
+#include "Minuit2/MnPrint.h"
+
+using namespace ROOT::Minuit2;
+
 int main() {
+
+ Quad4F fcn;
+
+//   PentiumTimer stopwatch;
+//   stopwatch.start();
+
+//   long long int start = stopwatch.lap().ticks();
+//   long long int stop = stopwatch.lap().ticks();
+//   std::cout<<"stop-start: "<<stop - start<<std::endl;
+//   start = stopwatch.lap().ticks();
+  {
+  //test constructor
+  MnUserParameters upar;
+  upar.Add("x", 1., 0.1);
+  upar.Add("y", 1., 0.1);
+  upar.Add("z", 1., 0.1);
+  upar.Add("w", 1., 0.1);
+
+  MnMigrad migrad(fcn, upar);
+  FunctionMinimum min = migrad();
+  std::cout<<"minimum: "<<min<<std::endl;
+  }
+  {
+     // use analytical derivatives 
+     Quad4FGrad gfcn;
+
+     MnUserParameters upar;
+     upar.Add("x", 1., 0.1);
+     upar.Add("y", 1., 0.1);
+     upar.Add("z", 1., 0.1);
+     upar.Add("w", 1., 0.1);
+
+     MnMigrad migrad(gfcn, upar);
+     FunctionMinimum min = migrad();
+     std::cout<<"minimum with grad calculation : "<<min<<std::endl;
+
+     // try to run hesse 
+     MnHesse hesse; 
+     hesse( gfcn, min); 
+     std::cout<<"minimum after hesse: "<<min<<std::endl;
+  }
+
+
+  
 
   // Experiment exp(0.1, 3.0, 10000);
   // exp.exportData();
   // exp.plot();
   // std::cout << "Integral: " << exp.integrate() << std::endl;
+
+
 
   Theory th;
   double var = th.Dimension0(1);
@@ -18,4 +73,9 @@ int main() {
   
   return 0;
 }
+
+
+
+
+
 
